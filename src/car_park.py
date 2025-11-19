647e5fc22b02923027353876c8fbca3de3012978
J107760 <j107760@tafe.wa.edu.au>
2025-11-19 08:56:28 +0800

    Set up Carpark, Sensor, Display classes in src

---
+++ src/car_park.py
@@ -0,0 +1,10 @@
+class CarPark:
+    def __init__(self, name, total_bays, sensors=None, displays=None):
+        super().__init__(bay_matrix=self.EMPTY)
+        self.name
+        self.total_bays()
+        self.available_bays()
+        self.parked_plates()
+        self.sensors
+        self.displays
+        

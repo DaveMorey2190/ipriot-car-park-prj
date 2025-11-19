6df2bc977e3c9442098b1bd8453049becc36d919
J107760 <j107760@tafe.wa.edu.au>
2025-11-19 09:28:51 +0800

    Carpark constructor and attributes

--- src/car_park.py
+++ src/car_park.py
@@ -1,10 +1,6 @@
 class CarPark:
-    def __init__(self, name, total_bays, sensors=None, displays=None):
-        super().__init__(bay_matrix=self.EMPTY)
-        self.name
-        self.total_bays()
-        self.available_bays()
-        self.parked_plates()
-        self.sensors
-        self.displays
-        
+    def __init__(self, location, capacity, plates = None, display = None):
+        self.location = location
+        self.displays = displays or []
+
+def __str__(self)
